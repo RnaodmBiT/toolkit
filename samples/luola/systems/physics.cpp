7d76d8ea527e2e46c0fb00289cb98357f82c342b
Ben Mitchell <rnaodm5@gmail.com>
2016-12-15 21:56:16 +1300

    Stop objects from falling off the screen

--- samples/luola/systems/physics.cpp
+++ samples/luola/systems/physics.cpp
@@ -7,5 +7,10 @@ void PhysicsSystem::update(float dt, EntityCollection& entities) {
         std::function<void(PositionComponent*, PhysicsComponent*)>([&] (PositionComponent* pos, PhysicsComponent* phys) {
         pos->position += phys->velocity * dt;
         phys->velocity.y += 100 * dt;
+
+        if (pos->position.y > 500) {
+            pos->position.y = 500;
+            phys->velocity.y = 0;
+        }
     }));
 }
